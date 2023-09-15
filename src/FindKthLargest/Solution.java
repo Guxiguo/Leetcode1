d925b2efa6bb7924b9dad78b366e1bf6354a3ae7
Guxiguo <2462517610@qq.com>
2023-09-15 13:35:06 +0800

    add removeNthFromEnd

---
+++ src/FindKthLargest/Solution.java
@@ -0,0 +1,25 @@
+package FindKthLargest;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+
+/**
+ * 给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+ *
+ * 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+ *
+ * 你必须设计并实现时间复杂度为 O(n) 的算法解决此问题。
+ */
+class Solution {
+    public static int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length-k];
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,5,6,4};
+        System.out.println(findKthLargest(nums,2));
+    }
+}
